74e6ba58bc0efd194a22fb75f6b17fdf0517f510
Piter920120 <rynek0@onet.eu>
2019-05-09 15:09:22 +0200

    add firebase ass datebase in app

---
+++ yacht-reservation/src/context/firebaseSetup.js
@@ -0,0 +1,14 @@
+
+  import firebase from 'firebase'
+  
+  var firebaseConfig = {
+    apiKey: "AIzaSyCIfrFGaEELAT7K2MJjweJgat5FhXnM18c",
+    authDomain: "yacht-reservation.firebaseapp.com",
+    databaseURL: "https://yacht-reservation.firebaseio.com",
+    projectId: "yacht-reservation",
+    storageBucket: "yacht-reservation.appspot.com",
+    messagingSenderId: "941296233568",
+    appId: "1:941296233568:web:e482944aa5ed4e3f"
+  };
+
+  firebase.initializeApp(firebaseConfig);
